607edb9a4aca478087f501367ad0d088ddc29d5e
Magret <belloabiodun17@gmail.com>
2024-06-13 17:30:13 +0000

    Task 7 solution resolved

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes
--- and store the average score for a student. Note: An average score can be a decimal
+-- and store the average score for a student. NB: An average score can be a decimal
 
 DELIMITER //
 
