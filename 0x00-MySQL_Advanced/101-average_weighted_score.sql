96fe7de4cbeb752d2e57ec57c27ecdaba85face1
Magret <belloabiodun17@gmail.com>
2024-06-13 19:05:39 +0000

    TAsk 13 solution resolved

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -5,39 +5,37 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE user_id INT;
-    DECLARE cur CURSOR FOR SELECT id FROM users;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-    -- Cursor to loop through each user
-    OPEN cur;
-
-    read_loop: LOOP
-        FETCH cur INTO user_id;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-
-        -- Declare variables to hold the computed total weighted score and total weight
+	DECLARE user_id INT;
         DECLARE total_weighted_score DECIMAL(10, 2);
         DECLARE total_weight INT;
+        DECLARE done INT DEFAULT FALSE;
+        DECLARE cur CURSOR FOR SELECT id FROM users;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
-        -- Compute the total weighted score and the total weight for the current user
-        SELECT SUM(c.score * p.weight), SUM(p.weight)
-        INTO total_weighted_score, total_weight
-        FROM corrections c
-        JOIN projects p ON c.project_id = p.id
-        WHERE c.user_id = user_id;
+    	-- Cursor to loop through each user
+    	OPEN cur;
 
-        -- Update the users table with the computed average weighted score
-        UPDATE users
-        SET average_score = IFNULL(total_weighted_score / total_weight, 0)
-        WHERE id = user_id;
+    	read_loop: LOOP
+        	FETCH cur INTO user_id;
+        	IF done THEN
+            		LEAVE read_loop;
+        	END IF;
 
-    END LOOP;
+        	-- Compute the total weighted score and the total weight for the current user
+        	SELECT SUM(c.score * p.weight), SUM(p.weight)
+        	INTO total_weighted_score, total_weight
+        	FROM corrections c
+        	JOIN projects p ON c.project_id = p.id
+        	WHERE c.user_id = user_id;
 
-    CLOSE cur;
+        	-- Update the users table with the computed average weighted score
+        	UPDATE users
+        	SET average_score = IFNULL(total_weighted_score / total_weight, 0)
+        	WHERE id = user_id;
+
+    	END LOOP;
+
+    	CLOSE cur;
 END //
 
 DELIMITER ;
